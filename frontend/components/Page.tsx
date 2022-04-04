dde35fba15428fc63b3255d23ba3651592d66859
Ruairidh Grass <ruripg@gmail.com>
2022-04-04 21:43:23 +0200

    added some more global styles

--- frontend/components/Page.tsx
+++ frontend/components/Page.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Header from "./Header";
 
 interface Props {
@@ -6,24 +6,65 @@ interface Props {
 }
 
 const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight: normal;
+  font-style:normal;
+}
   html{
     --black: #393939;
     --white:#FFFFFF; 
+    --offWhite: #ededed;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
     --darkBlue:#254085;
     --lightBlue:#1190CB;
     --pink:#DE0A82;
     --orange:#F47B21;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
   }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+    color: var(--orange);
+  }
+  button {
+    font-family: 'radnika_next',-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
 `
 
 const Page: React.FC<Props> = ({ children }) => (
   <div>
     <GlobalStyles />
     <Header />
-    <h2>I am the page component</h2>
-    {children}
+    <InnerStyles>
+      {children}
+    </InnerStyles>
   </div>
 )
 
